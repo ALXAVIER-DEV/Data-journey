595a1cb51aefc8318f73159da481770a2c6a0b5f
ALXAVIER-DEV <alexandre.l.xavier@outlook.com>
2026-03-10 20:20:31 -0300

    -------------------

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,6 +1,5 @@
 data "aws_s3_bucket" "existing_data_bucket" {
-  count  =  var.environment == "dev" ? 1 : 0
+  count  = var.create_data_bucket ? 0 : 1
   bucket = var.bucket_name
 }
-
 
--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "data_bucket" {
-  count  = var.environment == "dev" ? 1 : 0
+  count  = var.create_data_bucket ? 1 : 0
   bucket = var.bucket_name
   
   tags = {
