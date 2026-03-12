2314641c2ebaafb3e7246a28a9bb1a52075f3a85
ALXAVIER-DEV <alexandre.l.xavier@outlook.com>
2026-03-12 08:57:51 -0300

    ##################

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,6 +1,10 @@
 resource "aws_s3_bucket" "data_bucket" {
   count  = var.create_data_bucket ? 1 : 0
   bucket = var.bucket_name
+  lifecycle {
+  prevent_destroy = true
+  ignore_changes  = [bucket]  
+  }
   
   tags = {
     Name        = var.bucket_name
