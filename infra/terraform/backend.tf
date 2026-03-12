71ff7af870d082c119acd7cd42f13f30a28ddda9
ALXAVIER-DEV <alexandre.l.xavier@outlook.com>
2026-03-12 08:59:44 -0300

    ##########

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,9 +1,6 @@
 data "aws_s3_bucket" "existing_data_bucket" {
   count  = var.create_data_bucket ? 0 : 1
   bucket = var.bucket_name
-lifecycle {
-            prevent_destroy = true
-            ignore_changes  = [bucket]
-  }
+
 }
 
