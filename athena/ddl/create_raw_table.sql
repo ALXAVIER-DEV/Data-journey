2bbf4eee77a1ffa8f6a0a8b881d6765cccd0ac5a
ALXAVIER-DEV <alexandre.l.xavier@outlook.com>
2026-03-24 20:14:42 -0300

    DDL change

--- athena/ddl/create_raw_table.sql
+++ athena/ddl/create_raw_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS default.raw_bronze_messages (
-  message_id string,
-  environment string,
-  ingested_at string,
-  payload struct<hello:string,"from":string>,
-  source struct<source_type:string,topic_arn:string,subject:string,published_at:string>
+  message_id varchar,
+  environment varchar,
+  ingested_at varchar,
+  payload row(hello varchar, "from" varchar),
+  source row(source_type varchar, topic_arn varchar, subject varchar, published_at varchar)
 )
 PARTITIONED BY (date string)
 ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
