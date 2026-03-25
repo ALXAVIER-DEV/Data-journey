7154f12356b80377fdf7d3e34763895e14fa218e
ALXAVIER-DEV <alexandre.l.xavier@outlook.com>
2026-03-24 22:58:36 -0300

    fix dml ddl

--- athena/ddl/create_raw_table.sql
+++ athena/ddl/create_raw_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS default.raw_bronze_messages (
-  message_id varchar,
-  environment varchar,
-  ingested_at varchar,
-  payload row(hello varchar, "from" varchar),
-  source row(source_type varchar, topic_arn varchar, subject varchar, published_at varchar),
-  date varchar
+  message_id string,
+  environment string,
+  ingested_at string,
+  payload struct<hello:string,"from":string>,
+  source struct<source_type:string,topic_arn:string,subject:string,published_at:string>,
+  date string
 )
 WITH (
   external_location = 's3://dev-axcloud-lab-sa-east-1-data/bronze/raw/',
