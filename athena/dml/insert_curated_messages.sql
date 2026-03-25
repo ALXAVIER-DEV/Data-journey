82fecea47145d2a7a4b55feaa505eca1d80585c0
ALXAVIER-DEV <alexandre.l.xavier@outlook.com>
2026-03-25 10:25:10 -0300

    ddl fix

--- athena/dml/insert_curated_messages.sql
+++ athena/dml/insert_curated_messages.sql
@@ -2,8 +2,8 @@ CREATE TABLE default.raw_bronze_messages (
   message_id string,
   environment string,
   ingested_at string,
-  payload struct<hello:string,from:string>,
-  source struct<source_type:string,topic_arn:string,subject:string,published_at:string>,
+  payload row(hello varchar, "from" varchar),
+  source row(source_type varchar, topic_arn varchar, subject varchar, published_at varchar),
   date string
 )
 WITH (
--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -246,6 +246,7 @@ resource "aws_iam_role_policy" "glue_permissions" {
         Sid    = "S3Access"
         Effect = "Allow"
         Action = [
+          "s3:GetBucketLocation",
           "s3:GetObject",
           "s3:PutObject",
           "s3:DeleteObject",
@@ -273,8 +274,11 @@ resource "aws_iam_role_policy" "glue_permissions" {
         Effect = "Allow"
         Action = [
           "glue:GetDatabase",
+          "glue:GetDatabases",
           "glue:GetTable",
+          "glue:GetTables",
           "glue:GetPartitions",
+          "glue:BatchCreatePartition",
           "glue:CreateTable",
           "glue:UpdateTable"
         ]
